7338f01cd3d136c9be8edb9f14e5506a5f2f324e
Matheus Suffi <matheus@forip.com.br>
2017-01-05 16:56:49 -0200

    Criação do git_ignore

--- Controllers/index.js
+++ Controllers/index.js
@@ -13,12 +13,3 @@
 exports.index = function(){
 	return "Index";
 };
-exports.index2 = function(){
-	return "Index2";
-};
-exports.index3 = function(){
-	return "Index3";
-};
-exports.index4 = function(){
-	return "Index4";
-};
