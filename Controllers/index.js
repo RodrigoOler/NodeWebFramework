a1c0a76d277b6eaa6a856bb6065b44ad4657300f
Matheus Suffi <matheus@forip.com.br>
2017-01-05 16:49:34 -0200

    Primeiro Commit - Criação do Roteamento

---
+++ Controllers/index.js
@@ -0,0 +1,24 @@
+// module.exportss = {
+// 	index: function(){
+// 		return "Funciona !";
+// 	},
+// 	index2:function(){
+// 		return "Index2";
+// 	},
+// 	index3:function(){
+// 		return "Index3";
+// 	}
+//
+// }
+exports.index = function(){
+	return "Index";
+};
+exports.index2 = function(){
+	return "Index2";
+};
+exports.index3 = function(){
+	return "Index3";
+};
+exports.index4 = function(){
+	return "Index4";
+};
---
+++ Routes.js
@@ -0,0 +1,37 @@
+var http = require('http');
+//var Controller = require("./Controllers.js")
+http.createServer(function(req, res){
+	try{
+		res.setHeader('Access-Controll-Allow-Origin','*');
+		//Resolvendo o bug do favicon.ico
+		if (req.url === '/favicon.ico') {
+			res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+			res.end(/* icon content here */);
+		}else{
+			var url = req['url'].replace(/\s\n/g, '').split('/');
+			var controller = url[1];
+			var func = url[2];
+			var params = {};
+			var request_vars = {};
+			//Monta a URL para o controller. Tratando inclusive os parametros
+			if(url[2] != undefined){
+				params = func.split(/[&,?]+/);
+				func = params[0];
+				//Tratamento de URLs para montar um request até o controller
+				params.forEach(function(item_params){
+					if(item_params != func){
+						var objParams = item_params.split("=");
+						request_vars[objParams[0]] = objParams[1];
+					}
+				});
+			}
+			console.log("Vai entrar em!");
+			//Chamada do Controller passando os parametros
+			var append_controller = require("./Controllers/"+controller+".js");
+			res.write(append_controller[func]());
+		}
+	}catch(error){
+		console.log(error);
+	}
+	res.end();
+}).listen(3000);
