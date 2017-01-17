36e219ba374397c1491113da0ee9a9365047c4af
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-01-17 11:56:55 -0200

    Refactoring routes.js

--- routes.js
+++ routes.js
@@ -1,29 +1,36 @@
-var http = require('http');
-var helpers = require('./includes/Helpers.js');
-var fileSys = require('fs');
-http.createServer(function(req, res){
-	try{
-		res.setHeader('Access-Controll-Allow-Origin','*');
-		//Resolvendo o bug do favicon.ico
-		if (req.url === '/favicon.ico') {
-			res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-			res.end(/* icon content here */);
-		}else{
-			switch (req['method']) {
-				case "GET":
-					helpers.methodGet(req,res);
-					break;
-				case "POST":
-					helpers.methodPost(req,res);
-					break;
-				default:
-					helpers.methodGet(req,res);
-					break;
-			}
-		}
-	}catch(error){
-		console.log(error);
-		res.write(error);
-		res.end();
-	}
-}).listen(3000);
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const helpers = require('./includes/Helpers.js');
+
+const PORT = process.env.PORT | 3000
+
+http.createServer((request, response) => {
+  try {
+    response.setHeader('Access-Controll-Allow-Origin', '*');
+    //Resolvendo o bug do favicon.ico
+    if (request.url === '/favicon.ico') {
+      response.writeHead(200, {
+        'Content-Type': 'image/x-icon'
+      });
+      response.end( /* icon content here */ );
+    } else {
+      switch (request['method']) {
+        case "GET":
+          helpers.methodGet(request, response);
+          break;
+        case "POST":
+          helpers.methodPost(request, response);
+          break;
+        default:
+          helpers.methodGet(request, response);
+          break;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    response.write(error);
+    response.end();
+  }
+}).listen(PORT);
